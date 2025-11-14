e0442821126ad118fe08ff4e9b19c26398d2ea98
idkjustarandomdudeherenothingtosee <promcpegamer99@gmail.com>
2025-11-14 16:05:50 +0300

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,21 @@
+import { sign } from "./shared"
+
+export const config = { runtime: "edge" }
+
+export default async function handler(req) {
+  const { searchParams } = new URL(req.url)
+  const hwid = searchParams.get("hwid") || "none"
+
+  const expires = Math.floor(Date.now() / 1000) + 30 // 30 sec token
+  const payload = `${hwid}:${expires}`
+  const signature = sign(payload)
+
+  return new Response(
+    JSON.stringify({
+      token: signature,
+      expires,
+      payload
+    }),
+    { headers: { "Content-Type": "application/json" }}
+  )
+}
